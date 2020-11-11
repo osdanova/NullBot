0621a4037c953272d9e510d41ed2f4b66cbdb2c3
osdanova <osdanovaonline@outlook.com>
2020-11-11 20:22:28 +0100

    Comando informativo matinal
    Informativo matinal movido a comando

--- botNull.py
+++ botNull.py
@@ -13,6 +13,7 @@ import logging
 from random import randint
 import postgre 
 
+
 timeTexto = 0
 canTalk = True
 muchoTexto = 0
@@ -59,34 +60,33 @@ def echo(bot, update):
     global muchoTexto 
     global timeTexto 
 
-    mensaje = update.message
     mensajeLower = update.message.text.lower()
 
     #if (datetime.now() - timeTexto).total_seconds() < 60 and muchoTexto > 20:
     #   bot.sendDocument(chat_id=update.message.chat_id, document=open(dataPath + '/photo/texto.webp', 'rb'))
 
     #pajero
-    if datetime.now().hour == 22 and datetime.now().minute == 35  and searchString(mensajeLower, 'hora patito')):
+    if datetime.now().hour == 22 and datetime.now().minute == 35  and re.search(r'\bhora patito\b', mensajeLower):
         update.message.reply_text('Felicidades')
 
     #basicos
-    if update.message.text != None and "null stop" == update.message.text.lower():
+    if update.message.text != None and "null stop" == mensajeLower:
         stop(bot, update)
-    elif update.message.text != None and "null go" == update.message.text.lower():
+    elif update.message.text != None and "null go" == mensajeLower:
         restart(bot, update)
 
     #null
-    if searchString(mensajeLower, 'paja'):
+    if re.search(r'\bpaja\b', mensajeLower):
         now = datetime.now()
         if now.hour < 18 and now.hour > 8  and datetime.today().weekday() <= 4  :
             update.message.reply_text('Deja de hacerte pajas ' + update.message.from_user.name + ' en el trabajo')
         else:
             update.message.reply_text('Deja de hacerte pajas ' + update.message.from_user.name)
 
-    elif re.search(r'\bbuenos d[ií]as\b', update.message.text.lower()):
+    elif re.search(r'\bbuenos d[ií]as\b', mensajeLower):
         bot.sendDocument(chat_id=update.message.chat_id, document=open(dataPath + '/photo/buenos.webp', 'rb'))
 
-    elif searchString(update, 'top comentarios'):
+    elif re.search(r'\btop comentarios\b', mensajeLower):
         update.message.reply_text('''- Joe, no hay gow?
 - Me siento engañado, pense k Joseph era el de la gorra 
 - Donde Eren mate a Levi 
@@ -95,42 +95,47 @@ def echo(bot, update):
 - Akainu se parece a Luis Augusto
          ''')   
 
-    elif update.message.text.lower()[-3:] == 'ps5':
+    elif mensajeLower[-3:] == 'ps5':
         update.message.reply_text('POR EL CULO TE LA HINCO')
 
-    elif searchString(mensajeLower, xd') and randint(0,10) == 1 :
+    elif re.search(r'\bxd\b', mensajeLower) and randint(0,10) == 1:
         bot.sendDocument(chat_id=update.message.chat_id, document=open(dataPath + '/gifs/salvame.mp4', 'rb'))
 
-    elif re.search(r'\bnuevos cap[ií]tulos\b',update.message.text.lower()):
+    elif re.search(r'\bnuevos cap[ií]tulos\b',mensajeLower):
         rows = postgre.select("select * from new_cap order by id limit 5")
         for row in rows:
             bot.send_message(chat_id=update.message.chat_id, text='Titulo: '+row[0]+ ' Cap: '+ str(row[1])+' Url: '+row[2])
 
-    elif searchString(mensajeLower, 'busca anime'):
-        anime = update.message.text.lower().replace("busca anime", "").strip()
+    elif re.search(r'\bbusca anime\b', mensajeLower):
+        anime = mensajeLower.replace("busca anime", "").strip()
         rows = postgre.select("select * from anime where lower(titulo) like '"+anime+"%' limit 10")
         for row in rows:
             bot.send_message(chat_id=update.message.chat_id, text='Resultados: '+ row[0])
 
-    elif searchString(mensajeLower, 'busca cap'):
-        cap = update.message.text.lower().replace("busca cap", "").strip().split(" ", 1)
+    elif re.search(r'\bbusca cap\b', mensajeLower):
+        cap = mensajeLower.replace("busca cap", "").strip().split(" ", 1)
         rows = postgre.select("select * from capitulos where lower(anime) = '"+cap[1]+"' and episodio ="+cap[0])
         for row in rows:
             bot.send_message(chat_id=update.message.chat_id, text='Titulo: '+row[0]+ ' Cap: '+ str(row[1])+' Url: '+row[2])
 
-# Devuelve true si msg contiene str
-def searchString(msg, str):
-    return re.search(r'\b{re.escape(str)}\b', msg)
+# Imprime el informativo matinal
+def informativo(bot, update):
+    rows = postgre.select("select * from informativo where fecha = '"+ str(datetime.today())+"'")
+    if len(rows) == 0:
+         bot.send_photo(chat_id=update.message.chat_id, photo=open(dataPath + '/photo/informativo.jpg', 'rb'))
+    for row in rows:
+        bot.send_message(chat_id=update.message.chat_id, text=row[1])
 
 # Imprime capis de anime nuevos
 def animeCaps(bot, update, cantidad):
-    capis = postgre.select("select * from new_cap order by id limit " + cantidad)
+    capis = postgre.select("select * from new_cap order by id limit " + str(cantidad))
     for capi in capis:
-        printAnime(bot, update, row[0], str(row[1]), row[2])
+        printAnime(bot, update, capi[0], str(capi[1]), capi[2])
 
 # Imprime un mensaje de un capi formateado
 def printAnime(bot, update, titulo, capi, url):
-    bot.send_message(chat_id=update.message.chat_id, parse_mode='HTML', text="""<b>titulo</b><br>capi<br>url""")
+    #bot.send_message(chat_id=update.message.chat_id, parse_mode='HTML', text="""<b>titulo</b><p>capi<p>url""")
+    bot.send_message(chat_id=update.message.chat_id, text='Titulo: '+titulo+ ' Cap: '+ capi+' Url: '+ url)
 
 def isAdmin(bot, update):
     if str(update.message.from_user.id) == str(settings["main"]["admin"]) :
@@ -141,6 +146,10 @@ def isAdmin(bot, update):
 # COMANDO: imprime 5 capis nuevos
 def cmd_capis(bot, update):
     animeCaps(bot, update, 5)
+    
+# COMANDO: informativo matinal
+def cmd_informativo(bot, update):
+    animeCaps(bot, update)
       
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
@@ -170,7 +179,8 @@ def main():
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler('seguir', restart))
     dp.add_handler(CommandHandler('parar', stop))
-    dp.add_handler(CommandHandler("Capis", cmd_capis))
+    dp.add_handler(CommandHandler("capis", cmd_capis))
+    dp.add_handler(CommandHandler("informativo", cmd_informativo))
 
     dp.add_handler(MessageHandler(Filters.text, echo))
 
